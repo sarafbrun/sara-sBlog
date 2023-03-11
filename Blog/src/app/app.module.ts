ccf1b887a24076181a0717ce86b12a01e06a24aa
sarafbrun <sarafuen.93@gmail.com>
2023-03-11 12:25:40 +0100

    Creados los componentes

---
+++ Blog/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaPostsComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
