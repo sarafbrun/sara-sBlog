f6b389593f2bce060f09320e80bd0e51c0aea6ad
sarafbrun <sarafuen.93@gmail.com>
2023-03-11 13:25:58 +0100

    Creados interface y las rutas de los componentes

--- Blog/src/app/app-routing.module.ts
+++ Blog/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/posts' },
+  { path: 'posts', component: ListaPostsComponent },
+  { path: 'blog/new', component: FormularioComponent },
+  { path: '**', component: ListaPostsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
